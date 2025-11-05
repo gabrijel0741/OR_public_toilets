23571b336319bc79ddfef2c62757adeae82df054
gabrijel0741 <gabrijel0741@gmail.com>
2025-11-05 17:19:21 +0100

    correct data load

--- create_toilets.sql
+++ create_toilets.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS toilets (
-    toilet_id SERIAL PRIMARY KEY,
+    toilet_id INT PRIMARY KEY,
     name VARCHAR(50),
     address VARCHAR(80),
     city VARCHAR(50),
@@ -19,14 +19,6 @@ CREATE TABLE IF NOT EXISTS toilets (
     fee_amount NUMERIC(4,2)
 );
 
-CREATE TABLE IF NOT EXISTS reviews(
-    review_id SERIAL PRIMARY KEY,
-    review_user VARCHAR(50),
-    review_user_rating INT,
-    review_comment VARCHAR(255),
-    FOREIGN KEY (toilet_id) REFERENCES toilets(toilet_id) ON DELETE CASCADE
-)
-
 COPY toilets (
   toilet_id, name, address, city, postal_code, latitude, longitude, working_hours, unisex, baby_changing_station,
   toilets_for_disabled, regular_toilets, urinals, disabled_toilets, maintenance, last_date_renovated, 
@@ -34,6 +26,15 @@ COPY toilets (
 )
 FROM 'insert/path/to/csv_table_of_public_toilets_downloaded.csv'
 WITH (FORMAT csv, HEADER true, QUOTE '"', ESCAPE '"');
+
+CREATE TABLE IF NOT EXISTS reviews(
+    review_id SERIAL PRIMARY KEY,
+    review_user VARCHAR(50),
+    review_user_rating INT,
+    review_comment VARCHAR(255),
+    toilet_id INT,
+    FOREIGN KEY (toilet_id) REFERENCES toilets(toilet_id) ON DELETE CASCADE
+);
 
 COPY reviews (
   toilet_id, review_user, review_user_rating, review_comment
