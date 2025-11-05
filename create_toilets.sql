236c1b3b3d5a781c460a4ac4854f6964e82b53b1
gabrijel0741 <gabrijel0741@gmail.com>
2025-11-05 12:44:29 +0100

    splitting toilets and reviews

--- create_toilets.sql
+++ create_toilets.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS toilets (
-    id INT,
+    toilet_id SERIAL PRIMARY KEY,
     name VARCHAR(50),
     address VARCHAR(80),
     city VARCHAR(50),
@@ -16,18 +16,27 @@ CREATE TABLE IF NOT EXISTS toilets (
     maintenance VARCHAR(50),
     last_date_renovated DATE,
     free_of_charge BOOLEAN,
-    fee_amount NUMERIC(4,2),
-    average_review_rating NUMERIC(2,1),
+    fee_amount NUMERIC(4,2)
+);
+
+CREATE TABLE IF NOT EXISTS reviews(
+    review_id SERIAL PRIMARY KEY,
     review_user VARCHAR(50),
     review_user_rating INT,
     review_comment VARCHAR(255),
-    PRIMARY KEY (id, review_user)
-);
+    FOREIGN KEY (toilet_id) REFERENCES toilets(toilet_id) ON DELETE CASCADE
+)
 
 COPY toilets (
-  id, name, address, city, postal_code, latitude, longitude, working_hours, unisex, baby_changing_station,
+  toilet_id, name, address, city, postal_code, latitude, longitude, working_hours, unisex, baby_changing_station,
   toilets_for_disabled, regular_toilets, urinals, disabled_toilets, maintenance, last_date_renovated, 
-  free_of_charge, fee_amount, average_review_rating, review_user, review_user_rating, review_comment
+  free_of_charge, fee_amount
 )
 FROM 'insert/path/to/csv_table_of_public_toilets_downloaded.csv'
+WITH (FORMAT csv, HEADER true, QUOTE '"', ESCAPE '"');
+
+COPY toilets (
+  toilet_id, review_user, review_user_rating, review_comment
+)
+FROM 'insert/path/to/csv_table_of_public_toilets_reviews_downloaded.csv'
 WITH (FORMAT csv, HEADER true, QUOTE '"', ESCAPE '"');
